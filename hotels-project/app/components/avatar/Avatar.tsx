0e4b282904083a378d8af9c4fe119c81f7fa7884
Arseniy <97445616+ghblacktrade@users.noreply.github.com>
2023-04-14 16:28:08 +0700

    googleAvatar_api_consentScreen_credentials

--- hotels-project/app/components/avatar/Avatar.tsx
+++ hotels-project/app/components/avatar/Avatar.tsx
@@ -1,13 +1,17 @@
 'use client'
 import React, {FC} from 'react';
-import {CiUser} from "react-icons/all";
+import Image from 'next/image'
 import {AvatarProps} from "@/app/components/avatar/avatar.interface";
 
 const Avatar :React.FC<AvatarProps> = ({src}) => {
     return (
-        <div >
-            <CiUser className='rounded-full' height='30' width='30'/>
-        </div>
+        <Image
+        className='rounded-full'
+        height='30'
+        width='30'
+        alt='Avatar'
+        src={src || '/images/avatarUser.png' }
+        />
     );
 };
 
